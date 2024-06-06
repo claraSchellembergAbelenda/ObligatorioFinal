82e76f3678e3ced9fd87a50764ee79b5f8cb3bf3
claraSchellembergAbelenda <cs314059@fi365.ort.edu.uy>
2024-06-06 19:39:51 -0300

    separacion de servidor y cliente

---
+++ Cliente/Deposito.Presentacion/Models/ArticuloModel.cs
@@ -0,0 +1,12 @@
+﻿namespace Deposito.Presentacion.Models
+{
+    public class ArticuloModel
+    {
+        public string nombre { get; set; }
+        public string descripcion { get; set; }
+        public string codProveedor { get; set; }
+        public double precioActual { get; set; }
+        public int stock { get; set; }
+        public int id { get; set; }
+    }
+}
---
+++ Cliente/Deposito.Presentacion/Models/UsuarioModel.cs
@@ -0,0 +1,13 @@
+﻿namespace Deposito.Presentacion.Models
+{
+    public class UsuarioModel
+    {
+        public int Id { get; set; }
+        public string email { get; set; }
+        public string password { get; set; }
+        public string nombre { get; set; }
+        public string apellido { get; set; }
+        public bool esAdmin { get; set; }
+        public bool esEncargado { get; set; }
+    }
+}
